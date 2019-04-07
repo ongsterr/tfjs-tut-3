02d759e53098ba7dda5deef001deb01cfbca08e9
Chris Ong <ong.chris11@gmail.com>
2019-04-07 14:39:38 +1000

    feat: add functions to train model and test predictions

--- src/App.js
+++ src/App.js
@@ -1,28 +1,132 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import * as tf from '@tensorflow/tfjs'
+import * as tfvis from '@tensorflow/tfjs-vis'
+
+import houseData from './data.js'
 
 class App extends Component {
+  async componentDidMount() {
+    await this.run()
+  }
+
+  createModel() {
+    const model = tf.sequential()
+
+    model.add(tf.layers.dense({ inputShape: [1], units: 1, useBias: true }))
+    model.add(tf.layers.dense({ units: 1, useBias: true }))
+
+    return model
+  }
+
+  convertToTensor(data) {
+    return tf.tidy(() => {
+      // Step 1. Shuffle the data
+      tf.util.shuffle(data)
+
+      const inputs = data.map(d => d.noOfBedroom)
+      const labels = data.map(d => d.housePrice)
+
+      // Step 2: Convert data to tensor
+      const inputTensor = tf.tensor1d(inputs)
+      const labelTensor = tf.tensor1d(labels)
+
+      return {
+        inputs: inputTensor,
+        labels: labelTensor,
+      }
+    })
+  }
+
+  async trainModel(model, inputs, labels) {
+    //Prepare model for training
+    model.compile({
+      optimizer: 'sgd',
+      loss: 'meanSquaredError',
+      metrics: 'mse',
+    })
+
+    const batchSize = 20
+    const epochs = 20
+
+    return await model.fit(inputs, labels, {
+      batchSize,
+      epochs,
+      shuffle: true,
+      callbacks: tfvis.show.fitCallbacks(
+        { name: 'Training Performance' },
+        ['loss', 'mse'],
+        { height: 300, callbacks: ['onEpochEnd'] }
+      ),
+    })
+  }
+
+  testModel(model, inputData) {
+    const [xs, preds] = tf.tidy(() => {
+      const xs = tf.linspace(0, 20, 20)
+      const preds = model.predict(xs.reshape([20, 1]))
+
+      return [xs.dataSync(), preds.dataSync()]
+    })
+
+    const predictedPoints = Array.from(xs).map((val, i) => {
+      return { x: val, y: preds[i] }
+    })
+    console.log(predictedPoints)
+
+    const originalPoints = inputData.map(d => ({
+      x: d.noOfBedroom,
+      y: d.housePrice,
+    }))
+
+    tfvis.render.scatterplot(
+      { name: 'Model Predictions vs Original Data' },
+      {
+        values: [originalPoints, predictedPoints],
+        series: ['original', 'predicted'],
+      },
+      {
+        xLabel: 'No of Bedroom',
+        yLabel: 'House Price',
+        height: 300,
+      }
+    )
+  }
+
+  async run() {
+    const values = houseData.map(d => ({
+      x: d.noOfBedroom,
+      y: d.housePrice,
+    }))
+
+    // Render original data
+    tfvis.render.scatterplot(
+      { name: 'No of Bedrooms vs House Price' },
+      { values },
+      {
+        xLabel: 'No of Bedrooms',
+        yLabel: 'House Price',
+        height: 300,
+      }
+    )
+
+    // Create the model
+    const model = this.createModel()
+    tfvis.show.modelSummary({ name: 'Model Summary' }, model)
+
+    const tensorData = this.convertToTensor(houseData)
+    const { inputs, labels } = tensorData
+
+    // Train the model
+    await this.trainModel(model, inputs, labels)
+    console.log('Done training')
+
+    // Make some predictions using the model and compare them to original data
+    this.testModel(model, houseData)
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <div className="App">Hello Tensorflow</div>
   }
 }
 
-export default App;
+export default App
---
+++ src/data.js
@@ -0,0 +1,40 @@
+const houseData = [
+  {
+    noOfBedroom: 1,
+    housePrice: 1.0,
+  },
+  {
+    noOfBedroom: 2,
+    housePrice: 1.5,
+  },
+  {
+    noOfBedroom: 3,
+    housePrice: 2.0,
+  },
+  {
+    noOfBedroom: 4,
+    housePrice: 2.5,
+  },
+  {
+    noOfBedroom: 5,
+    housePrice: 3.0,
+  },
+  {
+    noOfBedroom: 6,
+    housePrice: 3.5,
+  },
+  {
+    noOfBedroom: 7,
+    housePrice: 4.0,
+  },
+  {
+    noOfBedroom: 8,
+    housePrice: 4.5,
+  },
+  {
+    noOfBedroom: 9,
+    housePrice: 5.0,
+  },
+]
+
+export default houseData
